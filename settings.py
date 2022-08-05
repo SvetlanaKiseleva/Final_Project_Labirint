9ce32efd173da685f99aae64d3884449e2adc2eb
SvetlanaKiseleva <svetlana_nhe@mail.ru>
2022-08-05 15:17:13 +0400

    'Files'

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+class Settings:
+    valid_gmail = "svetlanaqap@gmail.com"
+    valid_pass = "qap012345"
+    valid_name = "Svetlana"
+    phone_number = '+7927*******'
+    phone_number_wrong = '+78934059830900'
